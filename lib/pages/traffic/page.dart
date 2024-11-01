b9e333d49aba1bc11729c4d92765b2bff4066d4d
nkusikevin <nkusikvnhart@gmail.com>
2024-11-01 19:13:00 +0200

    making use of data not-found in traffic fines page

--- lib/pages/traffic/page.dart
+++ lib/pages/traffic/page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ipass_poc/components/traffic_fines_card.dart';
+import 'package:ipass_poc/components/no_data_found.dart';
 
 class TrafficFines extends StatefulWidget {
   const TrafficFines({super.key});
@@ -11,6 +12,7 @@ class TrafficFines extends StatefulWidget {
 class _TrafficFinesState extends State<TrafficFines> {
   List<String> selectedFinesIds = [];
   double totalAmount = 0;
+  final int itemCount = 7; 
 
   void _handleFineSelection(String fineId, bool isSelected, double amount) {
     setState(() {
@@ -37,93 +39,110 @@ class _TrafficFinesState extends State<TrafficFines> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Select all traffic fines you'd like to pay",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey.shade700,
+            if (itemCount > 0) ...{
+              Text(
+                "Select all traffic fines you'd like to pay",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey.shade700,
+                ),
               ),
-            ),
+            },
             const SizedBox(height: 20),
             Expanded(
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return TrafficFinesCard(
-                    offense: 'Over Speeding',
-                    vehicleNumber: 'RAD 123 A',
-                    amount: 25000,
-                    dueDate: DateTime(2024, 01, 30, 20, 0),
-                    dateTime: DateTime(2024, 01, 25, 20, 0),
-                    speed: 60,
-                    location: 'Nyarutarama, Green Hills Academy',
-                    imageUrl: 'assets/images/car.jpg',
-                    fineId: 'fine_$index',
-                    isSelected: selectedFinesIds.contains('fine_$index'),
-                    onSelected: _handleFineSelection,
-                  );
-                },
-              ),
+              child: _buildFinesList(),
             ),
             const SizedBox(height: 16),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Paying with :',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey.shade700,
+            if (itemCount > 0) ...[
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Paying with :',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey.shade700,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    '0780 000 000',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(width: 4),
+                    const Text(
+                      '0780 000 000',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    'Edit',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.purple,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(width: 4),
+                    const Text(
+                      'Edit',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            
-            TextButton(
-              onPressed: () {},
-              child: Container(
-                padding: const EdgeInsets.all(13),
-                width: double.infinity,
-                margin: const EdgeInsets.only(bottom: 20),
-                decoration: BoxDecoration(
-                  color: selectedFinesIds.isNotEmpty ? Colors.purple : Colors.grey,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Center(
-                  child: Text(
-                    'Pay ${totalAmount.toStringAsFixed(0)} RWF',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  ],
                 ),
               ),
-            )
+              TextButton(
+                onPressed: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(13),
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                    color: selectedFinesIds.isNotEmpty
+                        ? Colors.purple
+                        : Colors.grey,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Pay ${totalAmount.toStringAsFixed(0)} RWF',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildFinesList() {
+    if (itemCount == 0) {
+      return const NoDataFound(
+        title: 'No Traffic Fines Found',
+        description: 'Great news! You don’t have any outstanding traffic fines.',
+        imageIcon: 'assets/images/no_ticket.png',
+      );
+    }
+
+    return ListView.builder(
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return TrafficFinesCard(
+          offense: 'Over Speeding',
+          vehicleNumber: 'RAD 123 A',
+          amount: 25000,
+          dueDate: DateTime(2024, 01, 30, 20, 0),
+          dateTime: DateTime(2024, 01, 25, 20, 0),
+          speed: 60,
+          location: 'Nyarutarama, Green Hills Academy',
+          imageUrl: 'assets/images/car.jpg',
+          fineId: 'fine_$index',
+          isSelected: selectedFinesIds.contains('fine_$index'),
+          onSelected: _handleFineSelection,
+        );
+      },
     );
   }
 }
